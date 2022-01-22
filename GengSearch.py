7b1dee8a54f12a97239f8f2e1714d50f5fbf94bf
1121917292 <1121917292@qq.com>
2022-01-22 15:48:42 +0800

    update to 1.1.0

--- GengSearch.py
+++ GengSearch.py
@@ -6,6 +6,7 @@ import requests
 import time
 import simuse
 import os
+import json
 
 def log(SearchUrl='Error',infoUrl='Error',runingtime=0,target='None',mebmerID='None'):
     date=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
@@ -102,27 +103,39 @@ def Search(Searchinfo):
     finally:
         print('\nListing……')
 
-def Listing(data):
+def Listing(data,blacklist=0):
     #print('listing...')
-    message=simuse.Fetch_Message(data)
+    try:
+        message=simuse.Fetch_Message(data)
+    except:
+        print('Mirai未运行或未配置mah插件')
+        os.system('pause')
     SearchList=[]
     Searchdict={}
     if type(message)==type(0):
         #print('not')
         return SearchList
     for i in message:
+        sign=1
         checktext='none'
         if i['type']=='GroupMessage':
             messagechain_list=i['messagechain']
             messagechain_infodict=messagechain_list[1]
             if messagechain_infodict['type']=='Plain':
                 checktext=messagechain_infodict['text']
-        if checktext[:4]=='梗查询 ':
+            if blacklist!=0:
+                for k in blacklist['group']:
+                    if str(k)==str(i['group']):
+                        sign=0
+                for k in blacklist['member']:
+                    if str(k)==str(i['sender']):
+                        sign=0
+        if checktext[:4]=='梗查询 ' and sign==1:
             Searchdict.update(Searchkey=checktext[4:])
             Searchdict.update(Searchsender=i['sender'])
             Searchdict.update(Searchgroup=i['group'])
             SearchList.append(Searchdict.copy())
-        elif checktext=='来点梗':
+        elif checktext=='来点梗' and sign==1:
             Searchdict.update(Searchkey=' ')
             Searchdict.update(Searchsender=i['sender'])
             Searchdict.update(Searchgroup=i['group'])
@@ -130,9 +143,13 @@ def Listing(data):
     #print(SearchList)
     return SearchList        
 
-def GengSearch(data):
+def GengSearch(data,seting):
     SearchList=[]
-    SearchList=Listing(data)
+    blacklist=seting['blacklist']
+    if str(blacklist['switch'])==str(1):
+        SearchList=Listing(data,blacklist)
+    else:
+        SearchList=Listing(data)
     #print(SearchList)
     SearchListcheck=[]
     if SearchList!=SearchListcheck:
@@ -141,16 +158,62 @@ def GengSearch(data):
             imgpath=os.getcwd()
             imgpath=imgpath+r'\\temp\\Searchinfo_img.jpg'
             simuse.Send_Message(data,i['Searchgroup'],1,imgpath,2,path=1)
+
+def GetSeting():
+    setingfile=open('seting.json','r',encoding='utf-8')
+    seting=setingfile.read()
+    seting=json.loads(seting)
+    return seting
+
+def Checkset(seting):
+    senddaily=seting['senddaily']
+    blacklist=seting['blacklist']
+    if str(senddaily['switch'])==str(1):
+        print('已开启每日梗发送')
+        print('文本：',senddaily['text'])
+        print('发送时间为每日',senddaily['hour'],'点')
+        print('已开启发送的群：',senddaily['sendlist'])
+    else:
+        print('未开启每日发送')
+    if str(blacklist['switch'])==str(1):
+        print('已开启黑名单模式')
+        print('黑名单列表(群)：',blacklist['group'])
+        print('黑名单列表(成员)：',blacklist['member'])
+    else:
+        print('未开启黑名单模式')
     
+def Senddaily(data,senddaily):
+    hours=time.strftime("%H", time.localtime())
+    if str(senddaily['hour'])==hours :
+        for i in senddaily['sendlist']:
+            Searchdict={}
+            Searchdict.update(Searchkey=' ')
+            Searchdict.update(Searchsender='None')
+            Searchdict.update(Searchgroup=i)
+            Search(Searchdict)
+            simuse.Send_Message(data,i,1,senddaily['text'],1)
+            imgpath=os.getcwd()
+            imgpath=imgpath+r'\\temp\\Searchinfo_img.jpg'
+            simuse.Send_Message(data,i,1,imgpath,2,path=1)
+
 def main():
     data=simuse.Get_data()
     #print(data)
     data=simuse.Get_Session(data)
+    seting=GetSeting()
+    Checkset(seting)
     print(data)
     print('data Get Success')
     print('Listing……')
+    daysign=None
     while 1:
-        GengSearch(data)
+        senddaily=seting['senddaily']
+        if str(senddaily['switch'])==str(1):
+            day=time.strftime("%d", time.localtime())
+            if day!=daysign:
+                Senddaily(data,seting['senddaily'])
+                daysign=day
+        GengSearch(data,seting)
         time.sleep(2)
 
         
